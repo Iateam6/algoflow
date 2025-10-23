69da137b254d78114b63dc1020b052740fe06843
reggy0 <85451025+reggy0@users.noreply.github.com>
2025-10-23 13:28:45 +0300

    Update asgi.py

--- immigration_algoflow_APIs/asgi.py
+++ immigration_algoflow_APIs/asgi.py
@@ -11,8 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "immigration_algoflow_APIs.settings")
 
 application = get_asgi_application()
